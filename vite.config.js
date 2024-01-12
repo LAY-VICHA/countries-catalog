68be40849387416ccfa5aaf51a2048a1c12aae73
lay_vicha <layvicha081783923@gmail.com>
2024-01-12 20:29:28 +0700

    edit-vite-file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'country-catalog',
+  base: '/country-catalog/',
   plugins: [
     vue(),
   ]
