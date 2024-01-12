d25870ca08f4f601de0d4debad3588a4979af183
lay_vicha <layvicha081783923@gmail.com>
2024-01-12 15:06:40 +0700

    country-detail-popup

---
+++ src/components/CountryDetailModal.vue
@@ -0,0 +1,97 @@
+<script>
+
+export default {
+    data() {
+        return {
+            country: this.$parent.country,
+        };
+    },
+    props: {
+        togglePopup: Function,
+        popupTrigger: Object,
+    },
+    methods: {
+        close() {
+            this.togglePopup();
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full flex justify-center items-center
+        bg-white/[0.44] backdrop-opacity-[1.5]">
+        <div class="bg-white max-w-1/2">
+            <div
+                class="overflow-y-auto overflow-x-hidden justify-center items-center w-fullmd:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 max-h-full w-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-full">
+                        <button type="button"
+                            class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            data-modal-hide="progress-modal">
+                            <i @click="close()" class="fa-solid fa-xmark text-[#243673] text-[36px]"></i>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="p-4 md:p-5">
+                            <img class="h-[75px] text-gray-400 dark:text-gray-500 mb-4" :src="this.country.flags.png">
+                            <h3 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">{{ this.country.name.official
+                            }} ({{ this.country.cca2 }}/{{ this.country.cca3 }})</h3>
+                            <div class="grid grid-cols-2 gap-x-[50px]">
+                                <p class="text-gray-700 dark:text-gray-400 mb-1">Independent: {{ this.country.independent }}
+                                </p>
+                                <p class="text-gray-700 dark:text-gray-400 mb-1">Region: {{ this.country.region }}</p>
+                                <p class="text-gray-700 dark:text-gray-400 mb-1">Area: {{ this.country.area }}</p>
+                                <p class="text-gray-700 dark:text-gray-400 mb-1">Population: {{ this.country.population }}
+                                </p>
+                                <div class="mb-1">
+                                    <div v-for="(capital, id) in this.country.capital" :key="id">
+                                        <p class="text-gray-700 dark:text-gray-400">Capital: {{ capital }}</p>
+                                    </div>
+                                </div>
+                                <div class="mb-2">
+                                    <div class="text-gray-700 dark:text-gray-400 inline">Currencies: </div>
+                                    <div v-for="(currency, id, index) in this.country.currencies" :key="id" class="inline">
+                                        <p class="text-gray-700 dark:text-gray-400 inline">
+                                            <span v-if="index != 0">, </span>
+                                            {{ id }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-2">
+                                <div class="text-gray-700 dark:text-gray-400 inline">Timezone: </div>
+                                <div v-for="(timezone, id) in this.country.timezones" :key="id" class="inline">
+                                    <p class="text-gray-700 dark:text-gray-400 inline">
+                                        <span v-if="id != 0">, </span>
+                                        {{ timezone }}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="mb-2">
+                                <div class="text-gray-700 dark:text-gray-400 inline">Language: </div>
+                                <div v-for="(language, id, index) in this.country.languages" :key="id" class="inline">
+                                    <p class="text-gray-700 dark:text-gray-400 inline">
+                                        <span v-if="index != 0">, </span>
+                                        {{ language }}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="mb-2">
+                                <div class="font-semibold text-[18px] text-gray-700 dark:text-gray-400 mb-1">Native Country:</div>
+                                <div v-for="(nativeName, id) in country.name.nativeName" :key="id">
+                                    <p class="font-normal text-gray-700 dark:text-gray-400"><span class="font-semibold">{{ id }}:</span> {{
+                                    nativeName.official }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,33 @@
 <script>
 import axios from 'axios'
 import fuzzysort from 'fuzzysort';
+import CountryDetailModal from './CountryDetailModal.vue'
+import { ref, registerRuntimeCompiler } from 'vue'
 
 export default {
+  components: {
+    CountryDetailModal,
+  },
+
+  setup() {
+    var country = ref(null);
+
+    const popupTrigger = ref({
+      buttonTrigger: false,
+    })
+
+    const togglePopup = (trigger, clickedCountry) => {
+      popupTrigger.value[trigger] = !popupTrigger.value[trigger]
+      country.value = clickedCountry
+    }
+
+    return {
+      popupTrigger,
+      country,
+      togglePopup,
+    }
+  },
+
   data() {
     return {
       sortBy: 'sort_by',
@@ -10,7 +35,7 @@ export default {
       searchCountry: '',
       searchResult: [],
       totalCountry: '',
-      currentPage: 1,   
+      currentPage: 1,
       previousBtn: false,
       nextBtn: true,
       countryPerPage: 25,
@@ -31,13 +56,14 @@ export default {
 
       this.totalCountry = response.data.length
       this.gotoPage(1)
+      console.log(this.data);
     },
 
     search() {
-      if(this.searchCountry.trim() === '') {
+      if (this.searchCountry.trim() === '') {
         this.searchResult = this.data
         this.totalCountry = this.data.length
-        this.searchTotalPage = Math.ceil(this.totalCountry/this.countryPerPage)
+        this.searchTotalPage = Math.ceil(this.totalCountry / this.countryPerPage)
         this.gotoPage(1)
       } else {
         const options = {
@@ -52,49 +78,49 @@ export default {
 
         this.searchResult = sortedItems;
         this.totalCountry = sortedItems.length
-        this.searchTotalPage = Math.ceil(this.totalCountry/this.countryPerPage)
+        this.searchTotalPage = Math.ceil(this.totalCountry / this.countryPerPage)
         this.gotoPage(1)
       }
     },
 
     gotoPage(page) {
       let goto = 0
-      let until = this.countryPerPage 
+      let until = this.countryPerPage
 
-      if(page !== 1) {
-        goto = (page - 1) * this.countryPerPage 
+      if (page !== 1) {
+        goto = (page - 1) * this.countryPerPage
         until = goto + this.countryPerPage
       }
 
-      if(this.totalCountry < (page - this.currentPage) * this.countryPerPage) {
+      if (this.totalCountry < (page - this.currentPage) * this.countryPerPage) {
         this.displayCountry = this.searchResult.slice(goto, this.totalCountry)
       } else {
         this.displayCountry = this.searchResult.slice(goto, until)
       }
 
       this.currentPage = page
-      if(this.currentPage !== 1) {
+      if (this.currentPage !== 1) {
         this.previousBtn = true
       } else {
         this.previousBtn = false
       }
 
-      if(this.currentPage == Math.ceil(this.totalCountry/this.countryPerPage)) {
-        this.nextBtn = false 
+      if (this.currentPage == Math.ceil(this.totalCountry / this.countryPerPage)) {
+        this.nextBtn = false
       } else {
         this.nextBtn = true
       }
     },
 
     sortCountries() {
-      if(this.sortBy === "ASC") {
+      if (this.sortBy === "ASC") {
         let sortData = this.data
         let sort = sortData.slice().sort((a, b) => a.name.common.localeCompare(b.name.common));
         this.searchResult = sort
 
         this.totalCountry = sort.length
         this.gotoPage(1)
-      } else if(this.sortBy === "DESC") {
+      } else if (this.sortBy === "DESC") {
         let sortData = this.data
         let sort = sortData.slice().sort((a, b) => b.name.common.localeCompare(a.name.common));
         this.searchResult = sort
@@ -151,7 +177,8 @@ export default {
 
   <div class="px-[30px] py-[30px] grid grid-cols sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-[30px]">
     <div class="" v-for="data in displayCountry" :key="data.cnn3">
-      <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        @click="() => togglePopup('buttonTrigger', data)">
         <div class="w-full h-[170px] bg-[#eaeaea]">
           <img class="rounded-t-lg w-full h-full object-contain" :src="data.flags.png" alt="" />
         </div>
@@ -160,13 +187,16 @@ export default {
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ data.name.official }}</h5>
           <p class="mb-2 font-normal text-gray-700 dark:text-gray-400"><span class="font-semibold">cca2/cca3:</span> {{
             data.cca2 }}/{{ data.cca3 }}</p>
-          <p class="mb-2 font-normal text-gray-700 dark:text-gray-400"><span class="font-semibold">Calling Code:</span>
-          <div v-for="(suffixes, id) in data.idd.suffixes" :key="id" class="inline">
-            <span v-if="id != 0">, </span>
-            {{ data.idd.root }}{{ suffixes }}
-          </div>
+          <div class="mb-2">
+            <p class="font-normal text-gray-700 dark:text-gray-400"><span class="font-semibold">Calling Code:</span>
+            <div v-for="(suffixes, id) in data.idd.suffixes" :key="id" class="inline">
+              <span v-if="id != 0">, </span>
+              {{ data.idd.root }}{{ suffixes }}
+            </div>
           </p>
-
+          </div>
+          
+          <div class="font-semibold text-gray-700 dark:text-gray-400">Native name: </div>
           <div v-for="(nativeName, id) in data.name.nativeName" :key="id">
             <p class="font-normal text-gray-700 dark:text-gray-400"><span class="font-semibold">{{ id }}:</span> {{
               nativeName.official }}</p>
@@ -183,22 +213,53 @@ export default {
         </div>
       </div>
     </div>
+
+    <CountryDetailModal v-if="popupTrigger.buttonTrigger" :togglePopup="() => togglePopup('buttonTrigger')"
+      :country="country"></CountryDetailModal>
   </div>
 
   <div class="container md:mb-8 pt-8 px-4 mx-auto flex justify-center select-none">
-    <button :disabled="!previousBtn" :class="{ 'block border px-4 py-2 rounded-l hover:bg-gray-200 text-gray-600' : previousBtn, 'block border px-4 py-2 rounded-l text-gray-300' : !previousBtn }" @click="gotoPage(this.currentPage - 1)">&larr;</button>
+    <button :disabled="!previousBtn"
+      :class="{ 'block border px-4 py-2 rounded-l hover:bg-gray-200 text-gray-600': previousBtn, 'block border px-4 py-2 rounded-l text-gray-300': !previousBtn }"
+      @click="gotoPage(this.currentPage - 1)">&larr;</button>
 
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 1, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 1}" v-if="searchTotalPage >= 1" @click="gotoPage(1)">1</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 2, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 2}" v-if="searchTotalPage >= 2" @click="gotoPage(2)">2</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 3, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 3}" v-if="searchTotalPage >= 3" @click="gotoPage(3)">3</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 4, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 4}" v-if="searchTotalPage >= 4" @click="gotoPage(4)">4</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 5, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 5}" v-if="searchTotalPage >= 5" @click="gotoPage(5)">5</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 6, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 6}" v-if="searchTotalPage >= 6" @click="gotoPage(6)">6</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 7, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 7}" v-if="searchTotalPage >= 7" @click="gotoPage(7)">7</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 8, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 8}" v-if="searchTotalPage >= 8" @click="gotoPage(8)">8</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 9, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 9}" v-if="searchTotalPage >= 9" @click="gotoPage(9)">9</button>
-    <button :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 10, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 10}" v-if="searchTotalPage >= 10" @click="gotoPage(10)">10</button>
+    <button
+      :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600': currentPage !== 1, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white': currentPage === 1 }"
+      v-if="searchTotalPage >= 1" @click="gotoPage(1)">1</button>
+    <button
+      :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600': currentPage !== 2, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white': currentPage === 2 }"
+      v-if="searchTotalPage >= 2" @click="gotoPage(2)">2</button>
+    <button
+      :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600': currentPage !== 3, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white': currentPage === 3 }"
+      v-if="searchTotalPage >= 3" @click="gotoPage(3)">3</button>
+    <button
+      :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600': currentPage !== 4, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white': currentPage === 4 }"
+      v-if="searchTotalPage >= 4" @click="gotoPage(4)">4</button>
+    <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 5, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 5}"
+    v-if="searchTotalPage >= 5" @click="gotoPage(5)">5</button>
+  <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 6, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 6}"
+    v-if="searchTotalPage >= 6" @click="gotoPage(6)">6</button>
+  <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 7, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 7}"
+    v-if="searchTotalPage >= 7" @click="gotoPage(7)">7</button>
+  <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 8, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 8}"
+    v-if="searchTotalPage >= 8" @click="gotoPage(8)">8</button>
+  <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 9, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 9}"
+    v-if="searchTotalPage >= 9" @click="gotoPage(9)">9</button>
+  <button
+    :class="{ 'block border px-4 py-2 hover:bg-gray-200 text-gray-600' : currentPage !== 10, 'block border px-4 py-2 bg-indigo-500 hover:bg-indigo-400 text-white' : currentPage === 10}"
+    v-if="searchTotalPage >= 10" @click="gotoPage(10)">10</button>
 
-    <button :disabled="!nextBtn" :class="{ 'block border px-4 py-2 rounded-r hover:bg-gray-200 text-gray-600' : nextBtn, 'block border px-4 py-2 rounded-r text-gray-300' : !nextBtn }"  rel="next" @click="gotoPage(this.currentPage + 1)">&rarr;</button>
-  </div>
+  <button :disabled="!nextBtn"
+    :class="{ 'block border px-4 py-2 rounded-r hover:bg-gray-200 text-gray-600' : nextBtn, 'block border px-4 py-2 rounded-r text-gray-300' : !nextBtn }"
+    rel="next" @click="gotoPage(this.currentPage + 1)">&rarr;</button>
+</div>
 </template>
+
+<style>
+@import "../assets/style.css";
+</style>
